3dbca1323d26d4ad3c326ffd0cb08d9904ee5e85
胡德顺 <hudeshun@heytea.com>
2021-04-08 23:36:47 +0800

    JUC

--- src/main/java/com/cloud/k8s/springcloudk8s/juc/ThreadDemo.java
+++ src/main/java/com/cloud/k8s/springcloudk8s/juc/ThreadDemo.java
@@ -1,25 +1,71 @@
 package com.cloud.k8s.springcloudk8s.juc;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class ThreadDemo {
 
-    private static native void registerNatives();
-    static {
-        registerNatives();
+
+    /**
+     * 1、一个线程持有锁,另外一个线程获取这个锁，如果获取不到就处于block
+     * 2、或者在是调用wait方法后，再次获取锁
+     */
+    public static void block() throws InterruptedException {
+        ThreadDemo demo = new ThreadDemo();
+
+        final Thread thread = new Thread(() -> {
+            try {
+                synchronized (demo) {
+                    TimeUnit.SECONDS.sleep(10000L);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "1");
+        thread.start();
+
+        final Thread thread1 = new Thread(() -> {
+            synchronized (demo) {
+                try {
+                    demo.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "2");
+        thread1.start();
+
+        // 确定两个线程都启动
+        TimeUnit.SECONDS.sleep(2L);
+        log.info("线程1状态：{}", thread.getState());
+        log.info("线程2状态：{}", thread1.getState());
     }
 
 
-    private native void start0();
+    public static void strart() {
+        final Thread thread = new Thread(() -> {
+        });
 
+        thread.start();
 
-    public synchronized void start(){
-        start0();
+        log.info("线程状态：{}", thread.getState());
     }
 
 
+    public static void newState() {
+        final Thread thread = new Thread(() -> {
+            System.out.println("new");
+        });
 
-    public static void main(String[] args) {
-        ThreadDemo  threadDemo = new ThreadDemo();
-        threadDemo.start();
+        log.info("线程状态：{}", thread.getState());
+    }
 
+
+    public static void main(String[] args) throws InterruptedException {
+//        newState();
+//        strart();
+//        block();
     }
 }
