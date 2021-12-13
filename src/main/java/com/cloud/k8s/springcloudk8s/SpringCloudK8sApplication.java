b186294f7bfab90a55f4d3746ff8ce147c765c45
hudeshun <hudeshun@heytea.com>
2021-12-13 21:11:41 +0800

    CICD

--- src/main/java/com/cloud/k8s/springcloudk8s/SpringCloudK8sApplication.java
+++ src/main/java/com/cloud/k8s/springcloudk8s/SpringCloudK8sApplication.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Import;
 @Import({MyImportBeanDefinitionRegistrar.class, MyImportSelector.class})
 public class SpringCloudK8sApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudK8sApplication.class, args);
     }
