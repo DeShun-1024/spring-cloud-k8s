0409f7c0fc5c29f5dc4d6be6f243be5febea288d
hudeshun <hudeshun@heytea.com>
2022-01-29 18:09:41 +0800

    log

--- src/main/java/com/cloud/k8s/springcloudk8s/spring/lifecycle/MySmartLifecycle.java
+++ src/main/java/com/cloud/k8s/springcloudk8s/spring/lifecycle/MySmartLifecycle.java
@@ -1,13 +1,14 @@
 package com.cloud.k8s.springcloudk8s.spring.lifecycle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Configuration;
 
 
 /**
  * 在spring容器启动后，加载自己的组件信息
- *
  */
+@Slf4j
 @Configuration
 public class MySmartLifecycle implements SmartLifecycle {
 
@@ -18,11 +19,11 @@ public class MySmartLifecycle implements SmartLifecycle {
 
     @Override
     public void start() {
-        System.out.println("启动自己的组件");
+        log.info("自定义 【SmartLifecycle】启动自定义组件 执行方法 start()");
     }
 
     @Override
     public void stop() {
-        System.out.println("关闭自己的组件");
+        log.info("自定义 【SmartLifecycle】关闭自定义组件");
     }
 }
