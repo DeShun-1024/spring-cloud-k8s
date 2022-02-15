46ba91065e5c6175f3773bb6fe8c179180ab6b41
hudeshun <hudeshun@heytea.com>
2022-02-15 16:48:14 +0800

    log

--- src/main/java/com/cloud/k8s/springcloudk8s/juc/LockSupportDemo.java
+++ src/main/java/com/cloud/k8s/springcloudk8s/juc/LockSupportDemo.java
@@ -1,8 +1,11 @@
 package com.cloud.k8s.springcloudk8s.juc;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
+@Slf4j
 public class LockSupportDemo {
 
     public static void main(String[] args) throws InterruptedException {
@@ -10,13 +13,17 @@ public class LockSupportDemo {
         // 底层是调用unsafe的park，本地方法
         final Thread thread = new Thread(() -> {
             LockSupport.park();
-            System.out.println("11");
+            log.info("LockSupport.park()");
         }, "A");
         thread.start();
 
         TimeUnit.SECONDS.sleep(1);
-        new Thread(() -> {
-            LockSupport.unpark(thread);
-        }, "A9").start();
+//        new Thread(() -> {
+//            LockSupport.unpark(thread);
+//            log.info("LockSupport.unpark()");
+//        }, "A9").start();
+
+        // 唤醒后，才会执行
+        LockSupport.unpark(thread);
     }
 }
